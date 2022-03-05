e9e1046e69d134fdc42486a516d39f0b68343ad7
Ryan <rjlguitar@gmail.com>
2022-03-04 18:11:30 -0800

    added tab icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "react-native-vector-icons";
 
 function Bands() {
   return (
@@ -31,7 +32,26 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Bands") {
+              iconName = focused ? "headset" : "headset-outline";
+            } else if (route.name === "Stats") {
+              iconName = focused ? "stats-chart" : "stats-chart-outline";
+            } else if (route.name === "Styles") {
+              iconName = focused ? "musical-notes" : "musical-notes-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Bands" component={Bands} />
         <Tab.Screen name="Stats" component={Stats} />
         <Tab.Screen name="Styles" component={Styles} />
