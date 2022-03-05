345404bf9c456332e0eda62ebbcbd2cd71b801b0
Ryan <rjlguitar@gmail.com>
2022-03-04 17:57:18 -0800

    added nav tabs

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function Bands() {
+  return (
+    <View style={styles.container}>
+      <Text>Bands</Text>
+    </View>
+  );
+}
+
+function Stats() {
+  return (
+    <View style={styles.container}>
+      <Text>Stats</Text>
+    </View>
+  );
+}
+function Styles() {
+  return (
+    <View style={styles.container}>
+      <Text>Styles</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Bands" component={Bands} />
+        <Tab.Screen name="Stats" component={Stats} />
+        <Tab.Screen name="Styles" component={Styles} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
